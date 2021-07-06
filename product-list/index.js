97a6e648cd60eaba3a3ca6a8650afb945f3ffbe8
David Gonzalez <david.gonzalez@nearform.com>
2021-07-06 19:26:55 +0100

    This is the first version of the product list

---
+++ product-list/index.js
@@ -0,0 +1,32 @@
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+fastify.get('/products', async(request, reply) => {
+  const searchTerm = request.query['search']
+  const products = require('./products.json')
+  
+  if (!searchTerm) return products
+  
+  const filteredProducts = []
+  for(product of products) {
+    if (product.name.includes(searchTerm)){
+      filteredProducts.push(product)
+    }
+  }
+  return filteredProducts
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
