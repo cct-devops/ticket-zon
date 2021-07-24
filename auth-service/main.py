8507e6a711acb19a6adecb125c6727975c7caa6f
David Gonzalez <dagonza1983@gmail.com>
2021-07-24 12:02:14 +0100

    fixes

--- auth-service/main.py
+++ auth-service/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import request
+import os
+import sys
 import jwt
 import time
 
@@ -24,7 +26,7 @@ def jwt_issuer():
     for user in users:
         if user['username'] == username and user['password'] == password:
             now = int(time.time())
-            return jwt.encode({'application': "jwt-issuer"}, "my-super-super-secret", algorithm="HS256", headers = {"iss": username, "iat": now, "exp": now + 300})
+            return jwt.encode({'application': "jwt-issuer"}, os.environ["JWT_SECRET"], algorithm="HS256", headers = {"iss": username, "iat": now, "exp": now + 300})
             
     return {'status': 'Unauthorized'}, 401
 
@@ -32,10 +34,12 @@ def jwt_issuer():
 def jwt_verify():
     token = request.args.get('token')
     try:
-        return jwt.decode(token, "my-super-super-secret", algorithms=["HS256"])
+        return jwt.decode(token, os.environ["JWT_SECRET"], algorithms=["HS256"])
     except:
         return {'status': 'Unauthorized'}, 401
     
 
 if __name__ == '__main__':
+    if not "JWT_SECRET" in os.environ:
+        sys.exit("JWT_SECRET not set")
     app.run(host='0.0.0.0')
--- product-list/server.js
+++ product-list/server.js
@@ -5,32 +5,22 @@ module.exports = (opts) => {
   const jwt = require('jsonwebtoken')
 
   const validTokens = {}
-  
-  // Not used for now
-  function isTokenExpired(token) {
-    const tokenDecoded = jwt.decode(token, {complete: true})
-    return tokenDecoded.header.exp >= Date.now()
-  }
 
   fastify.get('/products', async (request, reply) => {
-    const auth = request.headers['authentication']
+    const auth = request.headers['authorization']
     if (!auth) {
       return reply.code(401).send({ status: 'Unauthorized' })
     }
     const token = auth.split(' ')[1]
-    // Verify token is valid
-    let decoded = {}
 
     // TODO: Check timestamp
-    if (!validTokens[token]) {
-      console.log('token was not found')
-      try{
-        await axios.get(`http://localhost:5000/jwt-verify?token=${token}`)
-        validTokens[token] = 1
-      } catch(e) {
-        console.log(e)
-        return reply.code(401).send({ status: 'Unauthorized' })
-      }
+    console.log('token was not found')
+    try{
+      await axios.get(`http://localhost:5000/jwt-verify?token=${token}`)
+      validTokens[token] = 1
+    } catch(e) {
+      console.log(e)
+      return reply.code(401).send({ status: 'Unauthorized' })
     }
 
     const searchTerm = request.query['search']
